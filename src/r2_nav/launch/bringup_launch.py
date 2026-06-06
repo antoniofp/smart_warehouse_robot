c56cf415e4a9a23d753642209d679d48129a580d
antoniofp <j.antoniofdez89@gmail.com>
2026-06-06 04:46:46 +0000

    Update default map to my_map

--- src/r2_nav/launch/bringup_launch.py
+++ src/r2_nav/launch/bringup_launch.py
@@ -61,7 +61,7 @@ def generate_launch_description():
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
-        default_value='/root/smart_warehouse_robot/src/r2_nav/maps/mapa_perfecto.yaml',
+        default_value='/root/smart_warehouse_robot/src/r2_nav/maps/my_map.yaml',
         description='Full path to map yaml file to load')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
